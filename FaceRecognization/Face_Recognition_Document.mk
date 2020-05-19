c0ab8f6ab92cfef427b054d5765fde9bdb7bea4b
Eswarappa <Asha.Eswarappa@avl.com>
2020-05-19 16:57:18 +0200

    Removed the additional file

--- FaceRecognization/Face_Recognition_Document.mk
+++
@@ -1,3 +0,0 @@
-The Face recognition files are built during the learning process in LinkedIn. Each files are put in different folder and output images are saved.
-
-Face Detection : A picture is taken and used the built in fucntion of python to detect the faces in the picture.
